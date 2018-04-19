66b7f4233926249ded882a82440ee514eb3580f7
Anna Buyanova <lightallloy@gmail.com>
2018-04-19 20:11:38 +0300

    Generate code for the days posts

---
+++ days.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+logo = '🌱💻'
+tag = '#spring2018@days_of_code'
+week_days = %w[пн вт ср чт пт сб вс]
+length = 21
+
+start = Date.parse('9.04.2018')
+1.upto(length) do |i|
+  day = (start + i - 1).strftime('%e.%m')
+  text = "#{logo} День #{i} (#{day}, #{week_days[(i-1)%7]})\n\n#{tag}"
+  puts text
+  puts "-------------------"
+end
+
+
